1ba4943fd0ad0ddde00812232fe8d969640921f2
Jules1991 <julianrosser91@gmail.com>
2015-09-02 17:21:08 +1000

    Added BirthdayListFragment

--- app/src/main/java/website/julianrosser/birthdays/MainActivity.java
+++ app/src/main/java/website/julianrosser/birthdays/MainActivity.java
@@ -1,5 +1,6 @@
 package website.julianrosser.birthdays;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +8,24 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+    BirthdayListFragment listFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Init FragmentManager
+        fragmentManager = getSupportFragmentManager();
+
+        // Get new ListFragment
+        listFragment = BirthdayListFragment.newInstance();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, listFragment)
+                .commit();
+
     }
 
     @Override
